c26f61edd8c2d90e321f61d7a80c17299e6cf52c
isaacbendixen <64161970+isaacbendixen@users.noreply.github.com>
2023-09-28 21:58:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Welcome to Isaac's Chatbot!\n")
+
+user_name = input("What is your name? ")
+user_age = input("How old are you? ")
+
+print("\n")
+
+choice = 0
+
+while True:
+  print("How can I help you today?\n")
+
+  print("1. Option 1\n2. Option 2\n3. Option 3\n4. Option 4\n5. Exit")
+  choice = int(input())
+  if choice == 5:
+    break
+
+print(f"\nGoodbye {user_name}. See you later!")
